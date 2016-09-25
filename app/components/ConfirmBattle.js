a17769031427bf158551a9a64b1c8a485a94528f
Alexey <grimzest@gmail.com>
2016-09-25 23:22:40 +0300

    initial

---
+++ app/components/ConfirmBattle.js
@@ -0,0 +1,49 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+var styles = require('../styles');
+var Link = require('react-router').Link;
+
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
+var MainContainer = require('./MainContainer');
+
+var Loading = require('./Loading');
+
+function ConfirmBattle(props){
+	return props.isLoading === true
+		? <Loading text='Waiting' speed={500}/>
+		: <MainContainer>
+			<h1>Confirm Players</h1>
+			<div className="row">
+				<div className="col-sm-8 col-sm-offset-2">
+					<UserDetailsWrapper header="Player One">
+						<UserDetails info={props.playersInfo[0]} />
+					</UserDetailsWrapper>
+					<UserDetailsWrapper header="Player Two">
+						<UserDetails info={props.playersInfo[1]} />
+					</UserDetailsWrapper>
+				</div>
+				<div className="col-sm-8 col-sm-offset-2">
+					<div className="col-sm-12" style={styles.space}>
+						<button className="btn btn-lg btn-success" type="button" onClick={props.onInitiateBattle}>
+							Initiate Battle
+						</button>
+					</div>
+					<div className="col-sm-12" style={styles.space}>
+						<Link to="/playerOne">
+							<button type="button" className="btn btn-lg btn-danger">Reselect Players</button>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</MainContainer>
+}
+
+ConfirmBattle.propTypes = {
+	isLoading: PropTypes.bool.isRequired,
+	onInitiateBattle: PropTypes.func.isRequired,
+	playersInfo: PropTypes.array.isRequired
+
+}
+
+module.exports = ConfirmBattle;
